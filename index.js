546e1470d8343f53a7cc44acb1115b9281e5efd2
despreston <despreston@gmail.com>
2017-07-07 18:23:36 -0400

    server setup

---
+++ index.js
@@ -0,0 +1,42 @@
+const config = require('./set-config');
+const routes = require('./lib/routes');
+const Hapi   = require('hapi');
+const server = new Hapi.Server();
+
+console.log(`
+  Starting server.
+  NODE_ENV: ${process.env.NODE_ENV}
+  port: ${config.port}
+`);
+
+server.connection({
+  port: config.port,
+  host: 'localhost'
+});
+
+server.register(require('vision'), err => {
+  if (err) {
+    console.log(err);
+  }
+
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    isCached: false,
+    relativeTo: __dirname,
+    path: 'lib/templates',
+    layout: 'default',
+    layoutPath: 'lib/templates/layouts',
+    partialsPath: 'lib/templates/partials'
+  });
+
+  server.route(routes);
+});
+
+server.on('response', request => {
+  console.log(
+  `${new Date()} ${request.method.toUpperCase()} ${request.url.path} --> ${request.response.statusCode}`);
+});
+
+server.start();
---
+++ set-config.js
@@ -0,0 +1,1 @@
+module.exports = require('./config.json')[process.env.NODE_ENV || 'dev'];
