9fd557729fc83dfe9a1bb5055d9ba38f1836e1c5
despreston <despreston@gmail.com>
2017-07-10 17:40:07 -0400

    first pass at design --skip-ci

--- tasks/sass.js
+++ tasks/sass.js
@@ -4,11 +4,11 @@ const rename      = require('gulp-rename');
 const browserSync = require('browser-sync').get('getbalance.com');
 
 module.exports = () => {
-  const input = 'lib/scss/**/*.scss';
+  const input = 'lib/scss/index.scss';
   const output = 'dist/';
 
   gulp.task('sass', () => {
-    return gulp.src('lib/**/*.scss')
+    return gulp.src(input)
       .pipe(sass({ outputStyle: 'compressed' }))
       .pipe(rename('styles.min.css'))
       .pipe(gulp.dest(output))
