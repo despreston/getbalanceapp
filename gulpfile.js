c312d90411a90912727831b38e701c28e709617d
despreston <despreston@gmail.com>
2017-07-10 21:03:14 -0400

    removing sass for now. digital ocean machine overloaded 😫  --skip-ci

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp        = require('gulp');
-const sass        = require('gulp-sass');
 const browserSync = require('browser-sync').create('getbalance.com');
 const config      = require('./set-config');
 const reload      = browserSync.reload;
@@ -9,7 +8,7 @@ const fnames = fs.readdirSync('./tasks/')
   .map(name => name.replace(/.js$/, ''))
   .forEach(task => require(`./tasks/${task}`)());
 
-gulp.task('default', ['server', 'sass']);
+gulp.task('default', ['server', 'css']);
 
 gulp.task('watch', ['default'], () => {
     browserSync.init({
@@ -18,7 +17,6 @@ gulp.task('watch', ['default'], () => {
     });
 
     gulp.watch(['lib/**/*.js', '*.js'], ['server']);
-    gulp.watch('lib/**/*.scss', ['sass']);
-    gulp.watch('lib/**/*.html').on('change', reload);
+    gulp.watch('lib/**/*').on('change', reload);
 });
 
